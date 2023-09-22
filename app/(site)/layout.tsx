37a3617f5fd81c79ea6cbf803fc595e9c6a8f0e8
Julian <juliansv22@gmail.com>
2023-09-22 11:25:40 -0300

    Delete unused files. Add flag img

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,8 +9,21 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Personal portfolio",
+	title: "JSV | Portfolio",
 	description: "Personal portfolio site",
+	openGraph: {
+		title: "JSV | Portfolio",
+		description: "Personal portfolio site",
+		type: "website",
+		locale: "en_IE",
+		url: "https://julisv.com",
+		images: [
+			{
+				url: "https://th.bing.com/th/id/R.8e6cf7cfbb67884efb493f47c4a41bf6?rik=2voy4R%2brYT99Bg&riu=http%3a%2f%2fimg.freeflagicons.com%2fthumb%2fglossy_round_icon%2fargentina%2fargentina_640.png&ehk=BfRIlRY%2brDGRZ1O%2bqBStnJ8wAsmBj1Kb%2fPy%2f2LKAWZY%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1",
+				alt: "JSV | Portfolio",
+			},
+		],
+	},
 };
 
 export default async function RootLayout({
