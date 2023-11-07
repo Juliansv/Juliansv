a8797c3768f7f263ee11ea8119428bc6653c0ba0
Juliansv <juliansv22@gmail.com>
2023-11-07 16:52:19 -0300

    Change gradient style and name position in cards

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,6 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 import "../globals.css";
 import { getPages } from "@/sanity/sanity.utils";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,15 +34,18 @@ export default async function RootLayout({
 }) {
 	const pages = await getPages();
 
+	// Fetch header image from DB
+
 	return (
 		<html lang="en">
 			<body className={`max-w-5xl mx-auto py-4 px-6 ${inter.className}`}>
 				<header className="flex items-center justify-between">
 					<Link
 						href="/"
-						className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 font-bold text-transparent bg-clip-text text-3xl"
+						className="bg-gradient-to-r from-blue-600 via-white-50 to-blue-500 font-bold text-transparent bg-clip-text text-3xl"
 					>
 						JSV
+						{/* Replace for image of sol de mayo */}
 					</Link>
 					<div className="flex items-center gap-5 text-sm text-gray-600">
 						{pages.map((page) => (
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,24 +7,24 @@ export const revalidate = 0;
 export default async function Home() {
 	const projects = await getProjects();
 
-	const handleClick = () => {
-		console.log("clicked");
-	};
+	console.log(projects[0].name);
+	
 
 	return (
 		<div>
 			<h1 className="text-7xl font-extrabold">
 				Hello I&apos;m
 				<span
-					className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
+					className="bg-gradient-to-r from-blue-700 via-white-50 to-blue-500
       bg-clip-text text-transparent"
 				>
 					{" "}
 					Julian!
 				</span>
 			</h1>
-			<p className="mt-3 text-xl text-gray-600">Check out my projects!</p>
-			<h2 className="mt-24 font-bold text-gray-700 text-3xl">My projects</h2>
+			<p className="mt-3 text-xl text-gray-600">I&apos;m an Argentinian Web Developer <br/>
+			¡Checkout my projects!</p>
+			<h2 className="mt-10 font-bold text-gray-700 text-3xl">My projects</h2>
 			<div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 				{projects.map((project) => (
 					<Link
@@ -32,6 +32,12 @@ export default async function Home() {
 						className="border-2 border-gray-500 rounded-lg p-1 flex flex-col items-center max-h-45 hover:scale-105 hover:border-blue-500 transition"
             href={project.url}
 					>
+						<div
+							className="mt-2 font-extrabold bg-gradient-to-r from-blue-800 via-blue-700 to-blue-500
+        bg-clip-text text-transparent"
+						>
+							{project.name}
+						</div>
 						{project.image && (
 							<Image
 								src={project.image}
@@ -41,12 +47,6 @@ export default async function Home() {
 								className="object-cover rounder-lg m-auto"
 							/>
 						)}
-						<div
-							className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
-        bg-clip-text text-transparent"
-						>
-							{project.name}
-						</div>
 					</Link>
 				))}
 			</div>
