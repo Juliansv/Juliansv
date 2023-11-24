04e0e5ab3f1fe9ec6a37c3a0556357ec3259b39a
Juliansv <juliansv22@gmail.com>
2023-11-24 18:24:09 -0300

    Create component projects-list

---
+++ app/(site)/components/projects-list.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Project } from "@/types/Project";
+
+interface ProjectsListProps {
+  data: Project[];
+}
+
+const ProjectsList: React.FC<ProjectsListProps> = ({ data }) => {
+  return (
+    <>
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {data.map((project: Project) => (
+          <Link
+            key={project._id}
+            className="border-2 border-gray-500 rounded-lg p-1 flex flex-col items-center hover:scale-105 hover:border-red-700 transition dark:bg-sky-50"
+            href={project.url}
+          >
+            <div
+              className="mt-2 font-extrabold bg-gradient-to-r from-sky-800 via-sky-700 to-sky-500
+          bg-clip-text text-transparent"
+            >
+              {project.name}
+            </div>
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={200}
+                height={80}
+                className="object-cover rounder-lg m-auto"
+              />
+            )}
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsList;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import { getProjects } from "@/sanity/sanity.utils";
 import Image from "next/image";
 import Link from "next/link";
+import ProjectsList from "./components/projects-list";
 
 export const revalidate = 0;
 
@@ -21,31 +22,7 @@ export default async function Home() {
 			</h1>
 			<p className="mt-3 text-xl text-gray-600 dark:text-sky-500">I&apos;m an Argentinian Web Developer</p>
 			<h2 className="mt-5 font-bold text-gray-700 text-3xl bg-clip-text dark:text-sky-100">Some of my projects</h2>
-			<div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-				{projects.map((project) => (
-					<Link
-						key={project._id}
-						className="border-2 bg-sky-50 border-gray-500 rounded-lg p-1 flex flex-col items-center hover:scale-105 hover:border-sky-700 dark:hover:border-red-700 transition"
-            href={project.url}
-					>
-						<div
-							className="mt-2 font-extrabold bg-gradient-to-r from-sky-700 via-sky-600 to-sky-700
-        bg-clip-text text-transparent"
-						>
-							{project.name}
-						</div>
-						{project.image && (
-							<Image
-								src={project.image}
-								alt={project.name}
-								width={200}
-								height={80}
-								className="object-cover rounder-lg m-auto"
-							/>
-						)}
-					</Link>
-				))}
-			</div>
+			<ProjectsList data={projects} />
 		</div>
 	);
 }
