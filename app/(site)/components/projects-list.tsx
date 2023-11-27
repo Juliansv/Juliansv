af29b72622f55a557980d7fd2237c42f5f1ea9fd
Juliansv <juliansv22@gmail.com>
2023-11-27 17:55:49 -0300

    Fix border color

--- app/(site)/components/projects-list.tsx
+++ app/(site)/components/projects-list.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Project } from "@/types/Project";
+import ProjectCard from "./project-card";
 
 interface ProjectsListProps {
   data: Project[];
@@ -11,27 +12,30 @@ const ProjectsList: React.FC<ProjectsListProps> = ({ data }) => {
     <>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {data.map((project: Project) => (
-          <Link
-            key={project._id}
-            className="border-2 border-gray-500 rounded-lg p-1 flex flex-col items-center hover:scale-105 hover:border-red-700 transition dark:bg-sky-50"
-            href={project.url}
-          >
-            <div
-              className="mt-2 font-extrabold bg-gradient-to-r from-sky-800 via-sky-700 to-sky-500
-          bg-clip-text text-transparent"
+          <>
+            {/* <ProjectCard project={project} /> */}
+            <Link
+              key={project._id}
+              className="border-2 border-gray-500 rounded-lg p-1 flex flex-col items-center hover:scale-105 hover:border-sky-700 hover:dark:border-red-700 transition dark:bg-sky-50"
+              href={project.url}
             >
-              {project.name}
-            </div>
-            {project.image && (
-              <Image
-                src={project.image}
-                alt={project.name}
-                width={200}
-                height={80}
-                className="object-cover rounder-lg m-auto"
-              />
-            )}
-          </Link>
+              <div
+                className="mt-2 font-extrabold bg-gradient-to-r from-sky-800 via-sky-700 to-sky-500
+            bg-clip-text text-transparent"
+              >
+                {project.name}
+              </div>
+              {project.image && (
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={200}
+                  height={80}
+                  className="object-cover rounder-lg m-auto"
+                />
+              )}
+            </Link>
+          </>
         ))}
       </div>
     </>
