bd6c96612fea59f4660b4ae9af798b6e724bf7f2
Juliansv <juliansv22@gmail.com>
2023-12-04 01:25:20 -0300

    fix theme button (wip)

--- app/(site)/components/dark-button.tsx
+++ app/(site)/components/dark-button.tsx
@@ -1,24 +1,21 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import { useTheme } from "next-themes";
-import { MdDarkMode, MdOutlineWbSunny } from "react-icons/md";
-
-
+// import { MdDarkMode } from "react-icons/md";
+// import { BsSun } from "react-icons/bs";
 
 const DarkButton = () => {
-    const { systemTheme, theme, setTheme } = useTheme();
-    const currentTheme = theme === 'system' ? systemTheme : theme;
+  const { theme, setTheme } = useTheme();
+  const currentTheme = theme;
 
-    return (
-        <button
-            onClick={() => theme == "dark"? setTheme('light'): setTheme("dark")}
-            className='bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-4 py-2 mx-2 text-md rounded-lg'>
-            {
-                currentTheme == "dark" ? <MdOutlineWbSunny /> : <MdDarkMode />
+  return (
+    <button
+      onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      className="bg-gray-800 dark:bg-gray-50 hover:bg-gray-600 dark:hover:bg-gray-300 transition-all duration-100 text-white dark:text-gray-800 px-4 py-2 mx-2 text-md rounded-lg"
+    >
+      {currentTheme == "dark" ? "<BsSun />" : "<MdDarkMode />"}
+    </button>
+  );
+};
 
-            }
-        </button>
-    )
-}
-
-export default DarkButton
+export default DarkButton;
