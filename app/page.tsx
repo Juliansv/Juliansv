6a475a2e1bc895e13d82cd2afc84bea21acbea43
Juliansv <juliansv22@gmail.com>
2024-04-24 02:45:08 +0200

    Add animation to archive link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { promises as fs } from "fs";
 import Link from "next/link";
 import Image from "next/image";
-import { Github, Linkedin } from "lucide-react";
+import { Github, Linkedin, ArrowRight } from "lucide-react";
+
 
 interface Job {
   period: string;
@@ -255,9 +256,19 @@ export default async function Page() {
               <div>
                 <Link
                   href="/archive"
-                  className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+                  className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group text-base"
                 >
-                  View Full Project Archive
+                  <span>
+                    <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+                      View Full Project {""}
+                    </span>
+                    <span>
+                      <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+                        Archive
+                      </span>
+                      <ArrowRight className="ml-1 inline-block h-4 w-4 shrink-0 transition-transform group-hover:translate-x-2 group-focus-visible:-translate-x-2" />
+                    </span>
+                  </span>
                 </Link>
               </div>
             </div>
