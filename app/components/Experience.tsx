0795b46aaae97e73bf42cfb12200cea49f9fcd7c
Juliansv <juliansv22@gmail.com>
2024-06-09 21:57:48 +0200

    Move everything into components

---
+++ app/components/Experience.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { FC } from "react";
+
+interface Job {
+  period: string;
+  position: string;
+  company: string;
+  description: string;
+  skills: string[];
+}
+
+interface JobProps {
+  experience: Job[];
+}
+
+const Experience: FC<JobProps> = ({experience}): JSX.Element => {
+
+  return (
+    <section
+      id="experience"
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+      aria-label="Work experience"
+    >
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-dark-purple/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+          Experience
+        </h2>
+      </div>
+      <div>
+        <ol className="group/list">
+          {experience.map((job, index) => (
+            <li key={index} className="mb-12">
+              <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+                  {job.period}
+                </header>
+                <div className="z-10 sm:col-span-6">
+                  <h3 className="font-medium leading-snug text-slate-200">
+                    {job.position + ` · ` + job.company}
+                  </h3>
+                  <p className="mt-2 text-sm leading-normal">
+                    {job.description}
+                  </p>
+                  <ul className="mt-2 flex flex-wrap">
+                    {job.skills.map((skill, index) => (
+                      <li key={index} className="mr-1.5 mt-2">
+                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+                          {skill}
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ol>
+        <div className="mt-12">
+          <Link
+            href="https://drive.google.com/file/d/13smCcgiBdyFLyl84QtOG0EQa4onTMIWr/view?usp=sharing"
+            className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+          >
+            View Full Resume
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
---
+++ app/components/Header.tsx
@@ -0,0 +1,70 @@
+import { Github, Linkedin } from "lucide-react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+      <div>
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+          <Link href="/">Julian Suarez Vivas</Link>
+        </h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+          Frontend Developer
+        </h2>
+        <p className="mt-4 max-w-sm leading-normal">
+          I focus on delivering slick, user-friendly apps with precision and
+          style, prioritizing user satisfaction above all.
+        </p>
+        <nav className="nav hidden lg:block" aria-label="In-page jump links">
+          <ul className="mt-16 w-max">
+            <li>
+              <Link href="#about" className="group flex items-center py-3">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  About
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#experience" className="group flex items-center py-3">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  Experience
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link href="#projects" className="group flex items-center py-3">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  Projects
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* Here it will go the nav links  */}
+      <ul className="ml-1 mt-8 flex items-center">
+        <li className="mr-5 text-xs shrink-0">
+          <Link
+            href="https://github.com/juliansv"
+            className="block hover:text-slate-200"
+          >
+            <Github />
+          </Link>
+        </li>
+        <li className="mr-5 text-xs shrink-0">
+          <Link
+            href="https://www.linkedin.com/in/juliansuarezvivas/"
+            className="block hover:text-slate-200"
+          >
+            <Linkedin />
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
