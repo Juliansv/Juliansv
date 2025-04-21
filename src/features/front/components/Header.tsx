ff9657a9936a6afeb7a9a599d766289afc063ded
Juliansv <juliansv22@gmail.com>
2025-04-21 17:01:55 -0300

    fix content displacement

--- src/features/front/components/Header.tsx
+++ src/features/front/components/Header.tsx
@@ -52,11 +52,11 @@ const Header = () => {
 							>
 								<span
 									id="about-nav-line"
-									className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-active:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 group-active:bg-slate-200 motion-reduce:transition-none"
+									className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-active:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 group-active:bg-slate-200 motion-reduce:transition-none line-active"
 								></span>
 								<span
 									id="about-nav-text"
-									className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200 group-active:text-slate-200"
+									className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200 group-active:text-slate-200 text-active"
 								>
 									About
 								</span>
